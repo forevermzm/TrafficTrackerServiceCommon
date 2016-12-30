accaab1e103a2d8dc8507bacf65eec2b4c621b8a
Zhemin Mi <forevermzm00@gmail.com>
2016-12-29 22:30:39 -0800

    Add more methods on SourceDestinationTableDAO

--- src/main/java/dao/SourceDestinationTableDAO.java
+++ src/main/java/dao/SourceDestinationTableDAO.java
@@ -1,8 +1,11 @@
 package dao;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import org.springframework.stereotype.Component;
 import pojo.dynamo.SourceDestinationPair;
+
+import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -16,5 +19,17 @@ public class SourceDestinationTableDAO {
 
     public void save(SourceDestinationPair pair) {
         mapper.save(pair);
+    }
+
+    public SourceDestinationPair read(String srcDestPair) {
+        return mapper.load(SourceDestinationPair.class, srcDestPair);
+    }
+
+    public List<SourceDestinationPair> scan(int limit) {
+        return mapper.scan(SourceDestinationPair.class, new DynamoDBScanExpression().withLimit(limit));
+    }
+
+    public void delete(SourceDestinationPair pair) {
+        mapper.delete(pair);
     }
 }
